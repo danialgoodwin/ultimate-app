76c68558a118e39b424ea8dbc1ea7669a89beb81
Danial Goodwin <danialgoodwin@gmail.com>
2020-02-29 03:37:38 -0700

    Add local storage for Notes

--- src/components/NotesCard.vue
+++ src/components/NotesCard.vue
@@ -40,11 +40,30 @@
 <script>
 export default {
   name: 'NotesCard',
+  props: {
+    storeId: String
+  },
+  mounted () {
+    if (localStorage[this.storeId + 'titleModel']) {
+      this.titleModel = localStorage[this.storeId + 'titleModel']
+    }
+    if (localStorage[this.storeId + 'contentModel']) {
+      this.contentModel = localStorage[this.storeId + 'contentModel']
+    }
+  },
   data: () => {
     return {
       isEditMode: true,
       titleModel: '',
       contentModel: ''
+    }
+  },
+  watch: {
+    titleModel (newValue) {
+      localStorage[this.storeId + 'titleModel'] = newValue
+    },
+    contentModel (newValue) {
+      localStorage[this.storeId + 'contentModel'] = newValue
     }
   },
   methods: {
--- src/views/NotesPage.vue
+++ src/views/NotesPage.vue
@@ -1,14 +1,17 @@
 <template>
   <v-row>
     <v-col cols="12" sm="4" md="3" lg="2">
-      <v-text-field name="search" label="Search" prepend-inner-icon="fas fa-search" solo flat hide-details></v-text-field>
+      <v-text-field name="search" label="Search" prepend-inner-icon="fas fa-search" solo flat
+                    hide-details></v-text-field>
       <v-list>
         <v-subheader>Pages</v-subheader>
         <v-btn color="primary" rounded>Add Page</v-btn>
         <template v-for="(item, index) in pages">
-          <v-divider v-if='item.isDivider' :key="index" />
+          <v-divider v-if='item.isDivider' :key="index"/>
           <v-list-item v-else :key="index" @click="doSomething">
-            <v-list-item-action v-if='item.icon'><v-icon>{{ item.icon }}</v-icon></v-list-item-action>
+            <v-list-item-action v-if='item.icon'>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item>
         </template>
@@ -16,12 +19,12 @@
     </v-col>
     <v-col cols="12" sm="8" md="9" lg="10">
       <v-row>
-        <notes-card />
-        <notes-card />
-        <notes-card />
-        <notes-card />
-        <notes-card />
-        <notes-card />
+        <notes-card store-id='notes-card-1'/>
+        <notes-card store-id='notes-card-2'/>
+        <notes-card store-id='notes-card-3'/>
+        <notes-card store-id='notes-card-4'/>
+        <notes-card store-id='notes-card-5'/>
+        <notes-card store-id='notes-card-6'/>
       </v-row>
       <v-row>
         <v-btn class="ma-2" color="primary" rounded>Add Note</v-btn>
@@ -32,24 +35,50 @@
 
 <script>
 import NotesCard from '@/components/NotesCard'
+
 export default {
   name: 'NotesPage',
   components: { NotesCard },
   data: () => ({
     pages: [
-      { title: 'Best Ideas', action: '' },
-      { title: 'Secrets', action: 'goToTextEditor' },
-      { title: 'Family', action: 'goToNotes' },
-      { title: 'Other', action: 'osChangeProfile' },
-      { title: 'TODOs', action: 'osSignOut' },
+      {
+        title: 'Best Ideas',
+        action: ''
+      },
+      {
+        title: 'Secrets',
+        action: 'goToTextEditor'
+      },
+      {
+        title: 'Family',
+        action: 'goToNotes'
+      },
+      {
+        title: 'Other',
+        action: 'osChangeProfile'
+      },
+      {
+        title: 'TODOs',
+        action: 'osSignOut'
+      },
       { isDivider: true },
-      { title: 'Settings', icon: 'fas fa-cog', action: 'showSettings' },
-      { title: 'Trash', icon: 'fas fa-trash-alt', action: 'showTrash' },
-      { title: 'Shortcuts', icon: 'fas fa-keyboard', action: 'showKeyboardShortcuts' }
+      {
+        title: 'Settings',
+        icon: 'fas fa-cog',
+        action: 'showSettings'
+      },
+      {
+        title: 'Trash',
+        icon: 'fas fa-trash-alt',
+        action: 'showTrash'
+      },
+      {
+        title: 'Shortcuts',
+        icon: 'fas fa-keyboard',
+        action: 'showKeyboardShortcuts'
+      }
     ],
-    actions: [
-
-    ]
+    actions: []
   }),
   methods: {
     doSomething: () => {
