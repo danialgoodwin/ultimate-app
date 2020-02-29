39999fd7ec8761fc9ac882412dba3b67b3bddcb3
Danial Goodwin <danialgoodwin@gmail.com>
2020-02-29 03:57:20 -0700

    Update TextEditorPage, including local storage

--- src/views/TextEditorPage.vue
+++ src/views/TextEditorPage.vue
@@ -1,21 +1,50 @@
 <template>
   <v-container>
-    <v-toolbar short>
-      <v-toolbar-title>My File Name</v-toolbar-title>
+    <v-toolbar short flat>
+      <v-toolbar-title><v-text-field v-model="titleModel" hide-details placeholder='Untitled'></v-text-field></v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn><v-icon>fas fa-bars</v-icon></v-btn>
+        <v-btn icon><v-icon>fas fa-bars</v-icon></v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <div>
-      asdfasdf
+      <v-textarea v-model="contentModel" outlined auto-grow autofocus></v-textarea>
     </div>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'TextEditorPage'
+  name: 'TextEditorPage',
+  props: {
+    // storeId: String
+    storeId: {
+      type: String,
+      default: '1'
+    }
+  },
+  mounted () {
+    if (localStorage[this.storeId + 'titleModel']) {
+      this.titleModel = localStorage[this.storeId + 'titleModel']
+    }
+    if (localStorage[this.storeId + 'contentModel']) {
+      this.contentModel = localStorage[this.storeId + 'contentModel']
+    }
+  },
+  data: () => {
+    return {
+      titleModel: '',
+      contentModel: ''
+    }
+  },
+  watch: {
+    titleModel (newValue) {
+      localStorage[this.storeId + 'titleModel'] = newValue
+    },
+    contentModel (newValue) {
+      localStorage[this.storeId + 'contentModel'] = newValue
+    }
+  }
 }
 </script>
 
